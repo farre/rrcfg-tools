28ea3d2328131e5fbe64e85aea852ce2387d2977
Simon Farre <simon.farre.cx@gmail.com>
2021-10-29 12:09:41 +0200

    Added rr commands factory function

---
+++ src/rrcommands.js
@@ -0,0 +1,28 @@
+const vscode = require("vscode");
+
+/** Registers subscriptions for rr commands
+ *  - reverse step
+ *  - reverse continue
+ * @param {string} type
+ */
+export function getCommands(type) {
+  let rev_continue = vscode.commands.registerCommand(`${type}.reverseContinue`, () => {
+    let expr = { expression: '-exec reverse-continue', context: 'repl' };
+    if(vscode.debug.activeDebugSession) {
+      vscode.debug.activeDebugSession.customRequest("evaluate", expr);
+    } else {
+      // TODO(simon): change so that this command is disabled, when no active debug session exists
+      vscode.window.showErrorMessage(`${type} command 'reverse continue' failed: No active debug session.`);
+    }
+  });
+  let rev_step = vscode.commands.registerCommand(`${type}.reverseStep`, () => {
+    let expr = { expression: '-exec reverse-step', context: 'repl' };
+    if(vscode.debug.activeDebugSession) {
+      vscode.debug.activeDebugSession.customRequest("evaluate", expr);
+    } else {
+      // TODO(simon): change so that this command is disabled, when no active debug session exists
+      vscode.window.showErrorMessage(`${type} command 'reverse step' failed: No active debug session.`);
+    }
+  });
+  return [rev_continue, rev_step];
+}
